8a41ccc88c0473a521b458d82da0bb459c038f03
Agustin Rodriguez <arodriguez@ivcisa.com>
2025-04-08 19:38:20 -0300

    resize de imagen

--- opencv/script.py
+++ opencv/script.py
@@ -1,1 +1,11 @@
 import cv2
+
+kame_house_image = cv2.imread("kame_house.jpg", 1)
+print(kame_house_image.shape)
+resize_kame = cv2.resize(kame_house_image, 
+    (int(kame_house_image.shape[1]/2), int(kame_house_image.shape[0]/2))) #el 0 es alto de la imagen, el 1 el ancho
+print(resize_kame.shape)
+
+#cv2.imshow("KameHouse", resize_kame)
+#cv2.waitKey(0)
+cv2.imwrite("kamehouse_r.jpg", resize_kame) #especificar extension
